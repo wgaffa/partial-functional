12a8aad0914e3f76b608c1d4fd45787a3606499b
Patrik Maunus <subscription@skriptladan.se>
2022-06-24 01:36:14 +0200

    Add Sum and Product monoid
    Sum is the monoid over Addition
    Product is the monoid over Multiplication

---
+++ src/monoid/product.rs
@@ -0,0 +1,93 @@
+use num_traits;
+
+use crate::semigroup::Semigroup;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Product<T>(pub T);
+
+impl<T: Semigroup + num_traits::Num> Default for Product<T> {
+    fn default() -> Self {
+        Self(num_traits::identities::One::one())
+    }
+}
+
+impl<T: PartialEq> PartialEq<T> for Product<T> {
+    fn eq(&self, other: &T) -> bool {
+        self.0 == *other
+    }
+}
+
+impl<T: PartialOrd> PartialOrd<T> for Product<T> {
+    fn partial_cmp(&self, other: &T) -> Option<std::cmp::Ordering> {
+        self.0.partial_cmp(other)
+    }
+}
+impl<T> From<T> for Product<T> {
+    fn from(value: T) -> Self {
+        Self(value)
+    }
+}
+
+impl<T: std::ops::Mul<Output = T>> Semigroup for Product<T> {
+    fn combine(self, rhs: Self) -> Self {
+        Self(self.0 * rhs.0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::monoid::Monoid;
+
+    use super::*;
+
+    use quickcheck::{Arbitrary, Gen, TestResult};
+    use quickcheck_macros::quickcheck;
+
+    impl Arbitrary for Product<u32> {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            Product(u8::arbitrary(g).into())
+        }
+
+        fn shrink(&self) -> Box<dyn Iterator<Item = Self>> {
+            Box::new(u32::shrink(&self.0).map(|x| x.into()))
+        }
+    }
+
+    #[quickcheck]
+    fn identity_property() {
+        assert_eq!(Product::<u32>(1), Product::empty())
+    }
+
+    fn property_product_of_vec_is_same_as_product_combine(vec: Vec<u8>) -> TestResult {
+        if vec.len() < 2 {
+            return TestResult::discard();
+        }
+
+        let left: Product<u128> = vec
+            .iter()
+            .copied()
+            .map(|x| x as u128)
+            .product::<u128>()
+            .into();
+        let right = vec
+            .iter()
+            .copied()
+            .map(|x| Product::from(x as u128))
+            .fold(Product::default(), |a, x| a.combine(x));
+
+        TestResult::from_bool(left == right)
+    }
+
+    #[test]
+    fn product_of_vec_is_same_as_product_combine() {
+        let gen = Gen::new(15);
+        let mut qtest = quickcheck::QuickCheck::new().gen(gen);
+
+        qtest.quickcheck(property_product_of_vec_is_same_as_product_combine as fn(Vec<u8>) -> TestResult);
+    }
+
+    #[quickcheck]
+    fn associativity_property(x: Product<u32>, y: Product<u32>, z: Product<u32>) -> bool {
+        x.combine(y.combine(z)) == x.combine(y).combine(z)
+    }
+}
--- src/monoid/sum.rs
+++ src/monoid/sum.rs
@@ -1,8 +1,13 @@
-use crate::{semigroup::Semigroup, monoid::Monoid};
-use monoid_derive::Semigroup;
+use crate::semigroup::Semigroup;
 
-#[derive(Debug, Semigroup, Monoid, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Sum<T>(pub T);
+
+impl<T: Default + Semigroup> Default for Sum<T> {
+    fn default() -> Self {
+        Self(Default::default())
+    }
+}
 
 impl<T: PartialEq> PartialEq<T> for Sum<T> {
     fn eq(&self, other: &T) -> bool {
@@ -19,5 +24,53 @@ impl<T: PartialOrd> PartialOrd<T> for Sum<T> {
 impl<T> From<T> for Sum<T> {
     fn from(value: T) -> Self {
         Self(value)
+    }
+}
+
+impl<T: std::ops::Add<Output = T>> Semigroup for Sum<T> {
+    fn combine(self, rhs: Self) -> Self {
+        Self(self.0 + rhs.0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::monoid::Monoid;
+
+    use super::*;
+
+    use quickcheck::Arbitrary;
+    use quickcheck_macros::quickcheck;
+
+    impl Arbitrary for Sum<u32> {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            Sum(u8::arbitrary(g).into())
+        }
+
+        fn shrink(&self) -> Box<dyn Iterator<Item = Self>> {
+            Box::new(u32::shrink(&self.0).map(|x| x.into()))
+        }
+    }
+
+    #[quickcheck]
+    fn identity_property() {
+        assert_eq!(Sum::<u32>(0), Sum::empty())
+    }
+
+    #[quickcheck]
+    fn sum_of_vec_is_same_as_sum_combine(vec: Vec<u8>) -> bool {
+        let left: Sum<u32> = vec.iter().copied().map(|x| x as u32).sum::<u32>().into();
+        let right = vec
+            .iter()
+            .copied()
+            .map(|x| Sum::from(x as u32))
+            .fold(Sum::default(), |a, x| a.combine(x));
+
+        left == right
+    }
+
+    #[quickcheck]
+    fn associativity_property(x: Sum<u32>, y: Sum<u32>, z: Sum<u32>) -> bool {
+        x.combine(y.combine(z)) == x.combine(y).combine(z)
     }
 }
