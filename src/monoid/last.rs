213b077019215a36bad70c7f73a9519c8c2e69fd
Patrik Maunus <subscription@skriptladan.se>
2022-06-23 22:11:45 +0200

    Add property tests to Last as Monoid

--- src/monoid/last.rs
+++ src/monoid/last.rs
@@ -1,6 +1,6 @@
 use crate::semigroup::Semigroup;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Last<T>(pub Option<T>);
 
 impl<T> Default for Last<T> {
@@ -30,5 +30,43 @@ impl<T> From<Last<T>> for Option<T> {
 impl<T> Semigroup for Last<T> {
     fn combine(self, rhs: Self) -> Self {
         Self(rhs.0.or(self.0))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::monoid::Monoid;
+
+    use super::*;
+
+    use quickcheck::Arbitrary;
+    use quickcheck_macros::quickcheck;
+
+    impl Arbitrary for Last<u32> {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            Last(Option::<u32>::arbitrary(g))
+        }
+
+        fn shrink(&self) -> Box<dyn Iterator<Item = Self>> {
+            Box::new(Option::<u32>::shrink(&self.0).map(|x| x.into()))
+        }
+    }
+
+    #[quickcheck]
+    fn identity_property() {
+        assert_eq!(Last::<u32>(None), Last::empty())
+    }
+
+    #[quickcheck]
+    fn last_is_set_to_the_last_non_none_value_in_a_list(vec: Vec<Last<u32>>) -> bool {
+        let last = vec.iter().copied().rfind(|x| *x != Last(None)).unwrap_or_default();
+        let right = vec.iter().copied().fold(Last::default(), |a, x| a.combine(x));
+
+        last == right
+    }
+
+    #[quickcheck]
+    fn associativity_property(x: Last<u32>, y: Last<u32>, z: Last<u32>) -> bool {
+        x.combine(y.combine(z)) == x.combine(y).combine(z)
     }
 }
